d17d0372a7c30c2a1b31199e7d61a0d454ec391f
Abhishek <iamabhishekaravind@gmail.com>
2025-10-11 16:03:10 +0530

    Update Mail

--- src/utils/razorpay.ts
+++ src/utils/razorpay.ts
@@ -6,14 +6,14 @@ declare global {
   }
 }
 
-const AMOUNT_PER_DEVOTEE = 200;
+const AMOUNT_PER_DEVOTEE = 1;
 
 export function loadRazorpayScript(): Promise<boolean> {
   return new Promise((resolve) => {
     if (window.Razorpay) {
       resolve(true);
       return;
-      
+
     }
 
     const script = document.createElement('script');
